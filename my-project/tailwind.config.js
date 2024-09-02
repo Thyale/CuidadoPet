be8e8590a197672b68e6da19a6f59795d8e2c59c
Thyale MD <thyaledantas@gmail.com>
2024-09-02 10:47:17 -0300

    Criação dos ajustes do tailwind

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode : "media",
   theme: {
+    colors: {
+      'Yellow': '#F2E205',
+      'Green': '#2E5F2E',
+      'Black' : '#0D0D0D',
+      'Brown' : '#8C6849',
+      'Bege': '#BFAE99',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      'Mobile': {'max': '639px'},
+    },
     extend: {},
   },
   plugins: [],
