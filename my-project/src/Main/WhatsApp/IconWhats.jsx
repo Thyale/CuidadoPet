7d3d9a4ec6c79a44d0adf0854072e4c3faa41e2a
Thyale MD <thyaledantas@gmail.com>
2024-09-03 10:58:52 -0300

    Animação do icon Whatsapp

--- my-project/src/Main/WhatsApp/IconWhats.jsx
+++ my-project/src/Main/WhatsApp/IconWhats.jsx
@@ -1,6 +1,18 @@
+import React, { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
+
 export default function IconWhats(){
+
+    useEffect(() => {
+        ScrollReveal().reveal('#Whats', {
+          origin: 'bottom',
+          distance: '20px',
+          duration: 1000,
+        });
+    }, []); 
+
     return(
-        <div className="bg-Green text-white px-5 py-4 rounded-full fixed bottom-2 right-2 cursor-pointer hover:bg-GreenDark">
+        <div id="Whats" className="bg-Green text-white px-5 py-4 rounded-full fixed bottom-2 right-2 cursor-pointer hover:bg-GreenDark">
             <a target="_Blanck" href="https://wa.me/5511999999999?text=Olá, Gostaria de mais informações!"><i className="fab fa-whatsapp text-4xl"></i></a>
         </div>
     )
