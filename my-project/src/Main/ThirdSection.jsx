1b02da9e68f52939bbd647a94ef82f0d2626b010
Thyale MD <thyaledantas@gmail.com>
2024-09-03 10:05:17 -0300

    Third Section Finalizada

--- my-project/src/Main/ThirdSection.jsx
+++ my-project/src/Main/ThirdSection.jsx
@@ -1,11 +1,26 @@
 export default function ThirdSection(){
     return(
-        <section>
-            <div>
-                <div>
-                    <i></i>
-                    <p></p>
-                    <p></p>
+        <section className="bg-[url('ImgFundoSectionThird.png')] bg-cover bg-no-repeat bg-center font-Frist">
+            <div className="flex flex-row gap-3 flex-wrap px-6 py-16 justify-around">
+                <div className="flex flex-col gap-2 items-center p-2">
+                    <i class="fas fa-smile text-white text-5xl"></i>
+                    <p className="text-Yellow text-4xl">5000</p>
+                    <p className="text-whiteTransparent">Clientes Satisfeitos</p>
+                </div>
+                <div className="flex flex-col gap-2 items-center p-2">
+                    <i class="fas fa-paw  text-white text-5xl"></i>
+                    <p className="text-Yellow text-4xl">3200</p>
+                    <p className="text-whiteTransparent">Animais Amados</p>
+                </div>
+                <div className="flex flex-col gap-2 items-center p-2">
+                    <i class="fas fa-stethoscope  text-white text-5xl"></i>
+                    <p className="text-Yellow text-4xl">4500</p>
+                    <p className="text-whiteTransparent">Atendimento Veterinário</p>
+                </div>
+                <div className="flex flex-col gap-2 items-center p-2">
+                    <i class="fas fa-award  text-white text-5xl"></i>
+                    <p className="text-Yellow text-4xl">3800</p>
+                    <p className="text-whiteTransparent">Reconhecimento</p>
                 </div>
             </div>
         </section>
