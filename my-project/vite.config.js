8688d6f26017bc4f3819feca7026f512ec2ff5ec
Thyale MD <thyaledantas@gmail.com>
2024-09-04 10:42:34 -0300

    Deploy

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CuidadoPet/',
   plugins: [react()],
+  build: {
+    publicDir: 'public',
+    assetsInclude: ['public/**/*'],
+  },
 })
